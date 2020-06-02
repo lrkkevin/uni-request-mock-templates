18b40ef04d6a2101b7f1c814c5f12f7a2f421e70
lrkkevin <lrkkevin@163.com>
2020-06-02 18:55:41 +0800

    ddd

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# uni-request-mock-templates
+easy-mock-template-axios
+---
+基于 axios 的接口定义模板。
+
+Documentation
+---
+[帮助文档](https://easy-mock.github.io/easy-mock-cli/)
---
+++ template/common/util.js
@@ -0,0 +1,41 @@
+function createAPI(baseURL) {
+    return function(conf) {
+        conf = conf || {};
+        // Promise
+        return new Promise((resolve, reject) => {
+            uni.showLoading();
+            uni.request({
+                url: baseURL + conf.url,
+                method: conf.method,
+                data: conf.opts,
+                success: res => {
+                    uni.hideLoading();
+                    resolve(res.data.data);
+                },
+                fail: err => {
+                    uni.hideLoading();
+                    console.log('err')
+                    reject(err)
+                }
+            })
+        }).catch(err => console.log(err))
+    };
+}
+
+function convertRESTAPI(url, opts) {
+    if (!opts || !opts.path) return url;
+
+    const pathKeys = Object.keys(opts.path);
+
+    pathKeys.forEach((key) => {
+        const r = new RegExp('(:' + key + '|{' + key + '})', 'g');
+        url = url.replace(r, opts.path[key]);
+    });
+
+    return url;
+}
+
+export {
+    createAPI,
+    convertRESTAPI
+};
